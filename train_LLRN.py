6030002262189c5a4b94c3cfda0c1bd87fce40ae
Danielgol <danielgol@yahoo.com.br>
2024-05-05 23:34:00 -0300

    ups

--- train_LLRN.py
+++ train_LLRN.py
@@ -283,12 +283,12 @@ def main():
     train_voc_loader = FilterSet([data for data in train_voc_loader if data[1]])
     # val_voc_loader = FilterSet([data for data in val_voc_loader if data[1]])
     print(len(val_voc_loader))
-    # count = 0
-    # for elem in val_voc_loader:
-    #     print(elem)
-    #     count += 1
-    #     if count == 10:
-    #         break
+    count = 0
+    for elem in val_voc_loader:
+        print(elem)
+        count += 1
+        if count == 10:
+            break
 
     train_data_loader = torch.utils.data.DataLoader(
         train_voc_loader,
